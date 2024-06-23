e6fe87bd44779e1d6dd47a20ab475ae93a73e2ce
Metsakal Zeleke <MetsakalZ@gmail.com>
2024-06-23 16:45:46 +0300

    Create pages and routes.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import LogIn from "./pages/LogIn";
+import SignUp from "./pages/SignUp";
+
 export default function App() {
-  return <div className="bg-red-700">Note taking app</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<Home />} />
+        <Route path={"/profile"} element={<Profile />} />
+        <Route path={"/login"} element={<LogIn />} />
+        <Route path={"/signup"} element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
