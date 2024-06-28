be4fb4881cd69d1e4bbc159a5b087a9048cd866b
Metsakal Zeleke <MetsakalZ@gmail.com>
2024-06-28 13:12:39 +0300

    Create user log in functionality.

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -39,3 +39,41 @@ export const userSignUp = [
     }
   },
 ];
+
+export const userLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "Incorrect email or password.",
+      });
+    }
+
+    const isPasswordSimilar = user.comparePassword(password);
+    if (!isPasswordSimilar) {
+      return res.status(401).json({
+        success: false,
+        message: "Incorrect email or password.",
+      });
+    }
+
+    const token = await user.generateToken();
+    const { password: pass, ...others } = user._doc;
+    res
+      .status(200)
+      .cookie("accessToken", token, { httpOnly: true })
+      .json({
+        success: true,
+        message: "User logged in successfully.",
+        data: { ...others, token },
+      });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Server error",
+    });
+  }
+};
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = mongoose.Schema(
   {
@@ -30,6 +31,15 @@ userSchema.pre("save", async function (next) {
   this.password = hashedPassword;
   next();
 });
+
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateToken = async function () {
+  const token = await jwt.sign({ id: this._id }, process.env.JWT_KEY);
+  return token;
+};
 
 const User = mongoose.model("User", userSchema);
 export default User;
