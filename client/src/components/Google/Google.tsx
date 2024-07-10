2254d7de2f829edd447232508fc50fd87659e3bf
Metsakal Zeleke <MetsakalZ@gmail.com>
2024-07-10 10:03:12 +0300

    Fix some issues on the svg part of the code.

--- client/src/components/Google/Google.tsx
+++ client/src/components/Google/Google.tsx
@@ -13,9 +13,9 @@ export default function Google() {
         <g
           id="Icons"
           stroke="none"
-          stroke-width="1"
+          strokeWidth="1"
           fill="none"
-          fill-rule="evenodd"
+          fillRule="evenodd"
         >
           <g id="Color-" transform="translate(-401.000000, -860.000000)">
             <g id="Google" transform="translate(401.000000, 860.000000)">
