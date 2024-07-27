44a9ba16e78c866092c5784d4ce313214510de00
Metsakal Zeleke <MetsakalZ@gmail.com>
2024-07-27 17:44:43 +0300

    Connect note creation functionality with the frontend.

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,24 +1,69 @@
-import { useState } from "react";
+import React, { useRef } from "react";
 import NoteInput from "../components/NoteInput";
 
 export default function Home() {
-  const [inputData, setInputData] = useState({
+  const titleRef = useRef<HTMLInputElement>(null);
+  const bodyRef = useRef<HTMLTextAreaElement>(null);
+
+  const note = {
     title: "",
     body: "",
-  });
-  console.log(inputData);
+  };
 
-  const handleChange = (
-    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    setInputData({ ...inputData, [event.target.id]: event.target.value });
+  const handleBlur = async (event: React.FocusEvent<HTMLFormElement>) => {
+    if (event.currentTarget.contains(event.relatedTarget as Node)) {
+      return;
+    }
+
+    event.preventDefault();
+
+    try {
+      if (titleRef.current !== null) {
+        if (titleRef.current?.value !== "") {
+          note.title = titleRef.current.value;
+        }
+      }
+
+      if (bodyRef.current !== null) {
+        if (bodyRef.current?.value !== "") {
+          note.body = bodyRef.current?.value;
+        }
+      }
+
+      const response = await fetch("http://localhost:3000/api/note/create", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(note),
+        credentials: "include",
+      });
+
+      const data = await response.json();
+
+      if (!data.success) {
+        console.log(data.message);
+        return;
+      } else {
+        console.log(data);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      if (titleRef.current !== null && bodyRef.current !== null) {
+        titleRef.current.value = "";
+        bodyRef.current.value = "";
+      }
+      note.title = "";
+      note.body = "";
+    }
   };
 
   return (
     <div className="mt-4">
       <div className="w-full flex justify-center items-center">
-        <form className="w-2/3">
-          <NoteInput inputData={inputData} onChange={handleChange} />
+        <form className="w-2/3" onBlur={handleBlur}>
+          <NoteInput titleRef={titleRef} bodyRef={bodyRef} />
         </form>
       </div>
     </div>
