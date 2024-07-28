cdd950856135147a22872397d3c57709fe4f4ff1
Metsakal Zeleke <MetsakalZ@gmail.com>
2024-07-28 18:43:34 +0300

    Create delete note route.

--- server/controllers/note.controller.js
+++ server/controllers/note.controller.js
@@ -1,5 +1,6 @@
 import { verifiedUser } from "../middlewares/verifiedUser.middleware.js";
 import Note from "../models/note.model.js";
+import Bin from "../models/bin.model.js";
 
 export const createNote = [
   verifiedUser,
@@ -27,6 +28,50 @@ export const createNote = [
         success: true,
         message: "Note created successfully.",
         data: note,
+      });
+    } catch (error) {
+      res.status(500).json({ success: false, message: "Server error" });
+    }
+  },
+];
+
+export const deleteNote = [
+  verifiedUser,
+
+  async (req, res) => {
+    try {
+      const { id } = req.params;
+      const creator = req.user.id;
+
+      const note = await Note.findById(id);
+
+      if (!note) {
+        return res.status(404).json({
+          success: false,
+          message: "Note not found.",
+        });
+      }
+
+      if (creator !== note.creator.toString()) {
+        return res.status(404).json({
+          success: false,
+          message: "Unauthorized action.",
+        });
+      }
+
+      const bin = await Bin.create({
+        title: note.title,
+        body: note.body,
+        creator: note.creator,
+      });
+
+      await Note.findByIdAndDelete(id);
+
+      return res.status(200).json({
+        success: true,
+        message:
+          "Note successfully put in bin. It will be automatically deleted in 30 days.",
+        data: bin,
       });
     } catch (error) {
       res.status(500).json({ success: false, message: "Server error" });
--- server/routes/note.route.js
+++ server/routes/note.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { createNote } from "../controllers/note.controller.js";
+import { createNote, deleteNote } from "../controllers/note.controller.js";
 
 const router = express.Router();
 
 router.post("/create", createNote);
+router.delete("/delete/:id", deleteNote);
 
 export default router;
