6dc5c48e33953c5397eff46d8adf2d4516fd80b3
Metsakal Zeleke <MetsakalZ@gmail.com>
2024-08-17 00:35:12 +0300

    Create get archive notes route.

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import { verifiedUser } from "../middlewares/verifiedUser.middleware.js";
 import Note from "../models/note.model.js";
 import Bin from "../models/bin.model.js";
+import Archive from "../models/archive.model.js";
 
 export const getNotes = [
   verifiedUser,
@@ -52,6 +53,36 @@ export const getBinNotes = [
         success: true,
         message: "Get request successful.",
         data: binNotes,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        success: false,
+        message: "Server error",
+      });
+    }
+  },
+];
+
+export const getArchiveNotes = [
+  verifiedUser,
+
+  async (req, res) => {
+    try {
+      const userId = req.user.id;
+
+      if (!userId) {
+        return res.status(403).json({
+          success: false,
+          message: "Unauthorized user.",
+        });
+      }
+
+      const archiveNote = await Archive.find({ creator: userId });
+
+      return res.status(200).json({
+        success: true,
+        message: "Get request successful.",
+        data: archiveNote,
       });
     } catch (error) {
       return res.status(500).json({
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { getNotes, getBinNotes } from "../controllers/user.controller.js";
+import {
+  getNotes,
+  getBinNotes,
+  getArchiveNotes,
+} from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.get("/get-notes", getNotes);
 router.get("/get-bin-notes", getBinNotes);
+router.get("/get-archive-notes", getArchiveNotes);
 
 export default router;
